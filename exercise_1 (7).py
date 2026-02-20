a3e6e1e1b5fc0b54ccae28849e5f2ee2aac5e8ca
mohammadali-code9 <0.warrior.1.0.0@gmail.com>
2026-02-20 13:27:46 +0330

    Update exercise_1 (7).py

--- exercise_1 (7).py
+++ exercise_1 (7).py
@@ -1,110 +1,92 @@
 class C_Queue:
     def __init__(self, max = 100):
-        self.list = [] * max
+        self.list = [None] * max
         self.front = -1
         self.rear = -1
-    def  insert(self , x):
-        if (self.rear +1) % len(self.list) == self.front:
+
+    def insert(self , x):
+        if (self.rear + 1) % len(self.list) == self.front:
             print("Queue is full")
             return
-        if  self.front == -1:
+        
+        if self.front == -1:
             self.front = 0
             self.rear = 0
             self.list[0] = x
             return
-        self.rear=(self.rear +1) % len(self.list)
+        
+        self.rear = (self.rear + 1) % len(self.list)
         self.list[self.rear] = x
+
     def delete(self):
         if self.front == -1:
             print("Queue is empty")
             return
+        
         if self.front == self.rear:
             k = self.list[self.front]
             self.front = -1
             self.rear = -1
             return k
+        
         k = self.list[self.front]
-        self.front = (self.front +1) % len (self.list)
+        self.front = (self.front + 1) % len(self.list)
         return k
+
     def is_empty(self):
         return self.front == -1
     
     def is_full(self):
-        return (self.rear +1) % len (self.list) == self.front
-    
+        return (self.rear + 1) % len(self.list) == self.front
+
+    # نمایش عناصر معتبر
     def show_valid(self):
+        if self.is_empty():
+            return
+        
         if self.rear >= self.front:
-            for i in range(self.front , self.rear+1 , 1):
+            for i in range(self.front , self.rear + 1):
                 print(self.list[i])
         else:
-            for i in range(self.front , len(self.list) , 1):
+            for i in range(self.front , len(self.list)):
                 print(self.list[i])
-            for i in range(self.rear +1):
+            for i in range(self.rear + 1):
                 print(self.list[i])
 
+    # نمایش خانه های غیرمعتبر
     def show_invalid(self):
-        if self.front == -1:
+        if self.is_empty():
             for i in range(len(self.list)):
-                print(self[i])
-                return
-            i = (self.rear+1) % len (self.list)
-            while i != self.front:
                 print(self.list[i])
-                i = (i +1) % len (self.list)
+            return
+        
+        i = (self.rear + 1) % len(self.list)
+        while i != self.front:
+            print(self.list[i])
+            i = (i + 1) % len(self.list)
 
+    # جستجو
     def Find(self , x):
         if self.is_empty():
             return
+        
         i = self.front
-        if self.list[i] == x:
-            return i
-        while i != self.rear:
-            i = (i +1) % len(self.list)
+        while True:
             if self.list[i] == x:
                 return i
-            
-    def raplace(self , x , y):
+            if i == self.rear:
+                break
+            i = (i + 1) % len(self.list)
+
+    # جایگزینی مقدار
+    def replace(self , x , y):
         if self.is_empty():
             return
-        i = self.front
-        if self.list[i] == x:
-            self.list[i] = y
-        while i != self.rear:
-            i = (i +1) % len (self.list)
-            if self.list[i] == x:
-                self.list[i] = y                   
-           
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         
-
-
-
-
-
+        i = self.front
+        while True:
+            if self.list[i] == x:
+                self.list[i] = y
+            if i == self.rear:
+                break
+            i = (i + 1) % len(self.list)
